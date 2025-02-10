84e45c218895dec5bebdf5791d1495cc02ce3483
José Eduardo Bandeira Júnior <83510462+JuniorBand@users.noreply.github.com>
2025-02-09 22:00:06 -0300

    Filter + Task

--- todolist/src/main/java/br/com/junior/second/todolist/tasks/TaskController.java
+++ todolist/src/main/java/br/com/junior/second/todolist/tasks/TaskController.java
@@ -1,11 +1,16 @@
 package br.com.junior.second.todolist.tasks;
 
+
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,7 +20,10 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        System.out.println("Chegou no controller");
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }    
